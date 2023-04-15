5a3490c7faac1ded9371d0a32c9fcaec886c31f9
paragSharma <parag0992.be21@chitkara.edu.in>
2023-04-15 12:18:32 +0530

    lect04-adding

--- react-learn/src/Expense.js
+++ react-learn/src/Expense.js
@@ -14,7 +14,18 @@ function Expense(props) {
   const filteredArray = props.a.filter((ex)=>{
     return ex.date.getFullYear().toString() === newYear ;
   })
-  
+  // coditional statements --->>>>
+
+  let newFilterArray ;
+  if(filteredArray.length === 0) 
+   newFilterArray = <p>No Expense Data</p>
+  else{
+   newFilterArray = filteredArray.map(ex=>{
+      console.log("alkdsjf");
+      return <ExpenseItem dat={ex.date} title={ex.title} amount={ex.amount}></ExpenseItem>
+    })
+  }
+
   return (
     <div>
       <ExpensesFilter
@@ -22,12 +33,25 @@ function Expense(props) {
         onClickYear={clickYear}
       ></ExpensesFilter>
       <div className="smallContainer">
-        {
-          filteredArray.map((ex)=>{
-               return <ExpenseItem dat ={ex.date} title={ex.title} amount={ex.amount} />
-          
+        {/* first method to do this-> use ternary instead of if else because of curly bractes  */}
+        {/* {
+          filteredArray.length === 0 ? <p>No Expense Found</p> : (
+            filteredArray.map((ex)=>{
+             return <ExpenseItem dat={ex.date} title={ex.title} amount={ex.amount}></ExpenseItem>
+            })
+          )
+        } */}
+
+        {/* 2nd method use abuse js -> when we && it return the  */}
+        {/* {filteredArray.length === 0 && <p>No Expense Found</p>}
+        {filteredArray.length > 0 && 
+          filteredArray.map(ex=>{
+            return <ExpenseItem dat={ex.date} title={ex.title} amount={ex.amount}></ExpenseItem>
           })
-        }       
+        } */}
+
+        {/* best way to do it */}
+        {newFilterArray}
         
       </div>
     </div>
