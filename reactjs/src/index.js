ecd19c35fd93c2b4da8b7977c7d64460dff8a20f
paragSharma <parag0992.be21@chitkara.edu.in>
2023-04-16 11:58:00 +0530

    updated

--- reactjs/src/index.js
+++
@@ -1,7 +0,0 @@
-import App from './App';
-import './index.css';
-
-import ReactDOM from 'react-dom/client';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
